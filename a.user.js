cf080ee1cb61799715a3edb40df671fbe2261fe3
hoanqson2107 <chidongianlason@gmail.com>
2026-01-22 20:39:41 +0700

    Create a.user.js

---
+++ a.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Auto Reload Tab
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  none
+// @match        https://studio.firebase.google.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    const RELOAD_INTERVAL = 60 * 1000;
+    setInterval(() => {
+        location.reload();
+    }, RELOAD_INTERVAL);
+})();
